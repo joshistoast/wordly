f1da8494255cf645c37eb55909964b7a0d234fdb
joshwcorbett <joshwcorbett@icloud.com>
2021-07-27 02:01:46 -0600

    Added Splash Screen

---
+++ src/components/Changelog.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="changelog" id="changelog">
+    <div class="changelog__header">
+      <span>{{ latestVersion.version }}</span>
+    </div>
+    <div class="changelog__content">
+      <div class="item" v-for="(change, i) in latestVersion.changes" :key="i">
+        • {{ change }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      versions: [
+        {
+          version: 'Beta 0.1',
+          changes: [
+            'Initial Release!',
+            'Please report any bugs to @joshwcorbett on Instagram'
+          ]
+        }
+      ]
+    }
+  },
+
+  computed: {
+    latestVersion () {
+      return this.versions.slice(-1).pop()
+    }
+  }
+}
+</script>
+
+<style lang='postcss' scoped>
+
+  .changelog {
+    display: inline-block;
+    background: var(--app-bg);
+    border: 1px solid rgba(#fff, 0.05);
+    border-radius: var(--global-radius);
+    color: white;
+    min-width: 350px;
+    width: 35vw;
+    max-width: 600px;
+    margin: 3em 0;
+    transition: background 120ms ease, box-shadow 200ms linear;
+
+    &:hover {
+      background: var(--dark-grey);
+      box-shadow: 0px 20px 35px rgba(#000,0.15);
+    }
+
+    &__header {
+      font-family: var(--font-sans-serif);
+      padding: 1.35em 2.25em;
+      width: 100%;
+      border-bottom: 1px solid var(--light-grey);
+      margin: 0;
+      font-weight: 700;
+      font-size: 1.2rem;
+    }
+    &__content {
+      font-family: var(--font-serif);
+      width: 65%;
+      margin: 2em auto;
+
+      .item {
+        display: block;
+        margin: 0.35em 0;
+        font-size: 1.2rem;
+      }
+    }
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,105 @@
 <template>
   <div class="home" id="home">
-    <router-link :to="{ name: 'App' }">Start Writing</router-link>
+    <div class="splash">
+      <div class="splash__header">
+        <h1>Wordly.</h1>
+        <span>Write your stories easier. Free.</span>
+      </div>
+      <Changelog></Changelog>
+      <div class="splash__buttons">
+        <a class="btn" :href="`${ githubLink }`" target="_blank">
+          <Icon icon='Github'/>
+        </a>
+        <router-link class="btn" :to="{ name: 'App' }">
+          <span>Start Writing</span>
+          <Icon icon='Arrow Right' />
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Changelog from '@/components/Changelog'
+import Icon from '@/components/Icon'
 export default {
+
+  data () {
+    return {
+      githubLink: 'https://github.com/joshwcorbett/wordly'
+    }
+  },
+
+  components: {
+    Changelog,
+    Icon
+  }
 
 }
 </script>
 
-<style>
+<style lang='postcss' scoped>
+
+  .home {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+  }
+
+  .splash {
+
+    &__header {
+      color: white;
+      text-align: center;
+
+      h1 {
+        font-family: var(--font-serif);
+        font-weight: 900;
+        font-size: 3.5rem;
+        margin-bottom: 0.25em;
+      }
+      span {
+        font-family: var(--font-sans-serif);
+        font-weight: 600;
+        font-size: 1.5rem;
+        opacity: 0.65;
+      }
+    }
+    &__buttons {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      margin-top: 3em;
+    }
+  }
+
+  .btn {
+    display: flex;
+    align-items: center;
+    background: var(--dark-grey);
+    border: 1px solid rgba(#fff, 0.05);
+    padding: 1em;
+    border-radius: var(--global-radius);
+    color: white;
+    text-decoration: none;
+    font-family: var(--font-serif);
+    font-weight: 700;
+    margin: 0.5em;
+    transition: box-shadow 200ms linear, background 50ms linear;
+    font-size: 1.35rem;
+
+    span {
+      margin-right: 0.35em;
+    }
+
+    &:hover {
+      background: var(--primary);
+      box-shadow: 0px 17px 30px rgba(#335EEA, 30%);
+    }
+  }
 
 </style>
